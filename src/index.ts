e31bed329bfd30eaed747c460e61fc561e9c63b8
Jesus <jesus.cassano.2006@gmail.com>
2023-01-31 18:17:43 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+export abstract class Character {
+  public isLive: boolean;
+  constructor(public name: string, public family: string, public age: number) {
+    this.isLive = true;
+  }
+
+  greetings() {
+    return `Hello, I am ${this.name}`;
+  }
+
+  die() {
+    this.isLive = false;
+  }
+}
+
+// export class Fighter {
+//   public
+// }
